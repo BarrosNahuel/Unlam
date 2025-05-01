c11d5956abf93f3282a92a98ccb5c5c71c08394c
BarrosNahuel <nabu.barros2004@gmail.com>
2025-05-01 16:30:12 -0300

    Update ListaSimplementeEnlazada.h

--- ListaSimplementeEnlazada/ListaSimplementeEnlazada.h
+++ ListaSimplementeEnlazada/ListaSimplementeEnlazada.h
@@ -5,5 +5,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct sNodo{
+    void* dato;
+    unsigned tam;
+    struct sNodo* sig;
+}tNodo;
+
+typedef tNodo* tLista;
+
+void crearLista(tLista* pLista);
+int ponerEnLista(tLista* pLista, const void* dato, unsigned tam);
+int sacarDeLista(tLista* pLista, void* dato, unsigned tam);
+int listaVacia(const tLista* pLista);
+void vaciarLista(tLista* lista);
+int mapLista(tLista* lista, void accion(tNodo* nodo));
+
 
 #endif // LISTASIMPLEMENTEENLAZADA_H_INCLUDED
